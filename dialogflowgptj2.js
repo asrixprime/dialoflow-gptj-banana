e7862e1af5713da7bba361052126a0329000509c
Abu <ashaikh.saad.as@gmail.com>
2022-11-13 16:36:28 +0800

    Update dialogflowgptj2.js

--- dialogflowgptj2.js
+++ dialogflowgptj2.js
@@ -9,38 +9,40 @@ const banana = require('@banana-dev/banana-dev');
 
 app.get('/', (req,res) => {
     res.send('Welcome to Dialogflow-GPTJ2 Application!!');
-  })
+})
 
 app.post("/dialogflow", express.json(), (req, res) => {
-const agent = new WebhookClient({ request: req, response: res });
-let intentMap = new Map();
-intentMap.set("Default Welcome Intent", welcome);
-intentMap.set("Default Fallback Intent", defaultFallback);
-agent.handleRequest(intentMap);
+    const agent = new WebhookClient({ request: req, response: res });
+    let intentMap = new Map();
+    intentMap.set("Default Welcome Intent", welcome);
+    intentMap.set("Default Fallback Intent", defaultFallback);
+    agent.handleRequest(intentMap);
 
-function welcome(agent) {
-    agent.add('Hi, I am Debra! I am your virtual personal assistant from Orient Telecoms. How are you doing today?');
-}
-
-async function defaultFallback(agent) {
-    let query = agent.query;
-    console.log('querytext: ', query)
-    var modelKey = "gptj"
-
-    const modelParameters = {
-        "text":query,
-        "length":50,
-        "temperature":0.9,
-        "batchSize": 1
+    function welcome(agent) {
+        agent.add('Hi, I am GPT-J! I am your virtual personal assistant from Orient Telecoms. How are you doing today?');
     }
 
-    try {
-        let run = async (modelParameters) => {
-            var botResponse = await banana.run(process.env.apiKey, modelKey, modelParameters)
-            
-            console.log('botResponse: ', botResponse)
-            agent.add(botResponse)
+    async function defaultFallback(agent) {
+        const run = async (query) => {
+        const modelKey = "gptj"
+    
+        const modelParameters = {
+            "text":query,
+            "length":50,
+            "temperature":0.9,
+            "batchSize": 1
         }
+        const out = await banana.run(process.env.apiKey, modelKey, modelParameters)
+        return out.modelOutputs[0].output.trim()
+    }
+
+    let query = agent.query;
+    console.log('querytext: ', query)
+
+    try {
+        const result = await run(JSON.stringify(query))
+        console.log('This is answer', result)
+        agent.add(result)
     } catch (err) {
         console.log('This is error:', err);
         agent.add('Sorry. Something went wrong. Can you say that again?');
